3ca6aca11302524d0b01575175f7ba97a5511eae
JmanCentral <jmancipet@ucentral.edu.co>
2026-02-19 14:20:44 -0500

    first commit

---
+++ src/const/const.js
@@ -0,0 +1,314 @@
+export const studies = [
+  {
+    title: "Ingeniero de sistemas",
+    corporation: "Universidad central (Bogotá) ",
+    date: "02/2021 – 12/2025",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Desarrollador web ",
+    corporation: "Asociación Profamilia",
+    date: "07/2025 – 31/2025",
+ },
+];
+    
+
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  springboot: "springboot",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  maven: "maven",
+  postgres: "postgres",
+  android: "android",
+  kotlin: "kotlin",
+  php:"php",
+  mySql: "mySql",
+  java:"java",
+  mapa:"mapa",
+  sqlite:"sqlite",
+  jwt:"jwt",
+  angular:"angular",
+  myBatis:"myBatis",
+  material:"material",
+  sql: "sql",
+  hibernate:"hibernate",
+  postman:"postman",
+  boostrap:"boostrap",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+    {
+    title: "bootstrap",
+    icon: IconKeys.boostrap,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "SpringBoot",
+    icon: IconKeys.springboot,
+  },
+  {
+    title: "java",
+    icon: IconKeys.java,
+  },
+  {
+    title: "JWT",
+    icon: IconKeys.jwt,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+    {
+    title: "Angular",
+    icon: IconKeys.angular,
+  },
+  {
+    title: "Angular Material",
+    icon: IconKeys.material,
+  },
+
+  {
+    title: "Php",
+    icon: IconKeys.php,
+  },
+  {
+    title: "MySQL",
+    icon: IconKeys.mySql,
+  },
+  {
+    title: "posgresql",
+    icon: IconKeys.postgres,
+  },
+  {
+    title: "SQLite",
+    icon: IconKeys.sqlite,
+  },
+  {
+    title: "SQL",
+    icon: IconKeys.sql,
+  },
+  {
+    title: "Hibernate",
+    icon: IconKeys.hibernate,
+  },
+
+  {
+    title: "MyBatis",
+    icon: IconKeys.myBatis,
+  },
+  {
+    title: "Android Studio",
+    icon: IconKeys.android,
+  },
+  {
+    title: "postman",
+    icon: IconKeys.postman,
+  },
+  {
+    title: "Kotlin",
+    icon: IconKeys.kotlin,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/finazapp.webp",
+    title: "FinazApp",
+    link: "https://drive.google.com/file/d/1wWqoGcJL_0RgXwBOY1INegBLSBvcTo4Y/view?usp=drive_link",
+    description: `Proyecto FullStack de un gestor de finanzas personales para móviles android utilizando una API de gemini para recomendaciones personalizadas.`,
+    technologies: [
+      {
+        icon: IconKeys.springboot,
+      },
+      {
+        icon: IconKeys.java,
+      },
+       {
+        icon: IconKeys.jwt,
+      },
+      {
+        icon: IconKeys.postgres,
+      },
+      {
+        icon: IconKeys.android,
+      },
+      {
+        icon: IconKeys.kotlin,
+      },
+    ],
+  },
+  {
+    img: "../../../img/factutiendas.webp",
+    title: "FactuTiendas",
+    link: "https://drive.google.com/file/d/17P0x1xHH2r7VXkDCD3_fQ49vKMDe92oz/view?usp=drive_link",
+    description: `plataforma web para facilitar la gestión de las operaciones comerciales de una pequeña o mediana empresa, abarcando desde el control de inventario y productos hasta la facturación y el seguimiento de clientes y proveedores.`,
+    technologies: [
+      {
+        icon: IconKeys.php,
+      },
+      {
+        icon: IconKeys.mySql,
+      },
+       {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+          {
+        icon: IconKeys.boostrap,
+      },
+    ],
+  },
+  {
+    img: "../../../img/gorunner.webp",
+    title: "GoRunner",
+    link: "https://drive.google.com/file/d/1tNsM0psh_6SLayDyj_-p35mr6IsG1Ndy/view?usp=drive_link",
+    description: `App mòvil para corredores que permite el rastreo de sus recorridos usando el gps de la API de google`,
+    technologies: [
+      {
+        icon: IconKeys.android,
+      },
+      {
+        icon: IconKeys.java,
+      },
+      {
+        icon: IconKeys.mapa,
+      },
+        {
+        icon: IconKeys.sqlite,
+      },
+    ],
+  },
+  {
+    img: "../../../img/triviaapp.webp",
+    title: "TriviaApp",
+    link: "https://drive.google.com/file/d/1u_Eyo8tbRbWxOrOBgfe6T5Vz7ok4zBCb/view?usp=drive_link",
+    description: `Aplicación móvil que simula un juego de preguntas desarrolada para android que permite a las personas poner a prueba sus conocimientos en diferentes áreas de interés.`,
+    technologies: [
+      {
+        icon: IconKeys.springboot,
+      },
+      {
+        icon: IconKeys.java,
+      },
+      {
+        icon: IconKeys.postgres,
+      },
+      {
+        icon: IconKeys.android,
+      },
+      {
+        icon: IconKeys.sqlite,
+      },
+    ],
+  },
+  {
+    img: "../../../img/universidad.webp",
+    title: "Gestor universidad",
+    link: "https://drive.google.com/file/d/1rQatvMYUarvJ9qG11MHPj742MmzsVh9u/view?usp=drive_link",
+    description: `aplicación pequeña de un sistema CRUD para una universidad que cuenta con profesores , cursos y estudiantes`,
+    technologies: [
+      {
+        icon: IconKeys.nodejs,
+      },
+      {
+        icon: IconKeys.mongodb,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript
+      },
+    ],
+  },
+
+  {
+    img: "../../../img/Profamilia.webp",
+    title: "presupuesto",
+    link: "../../../img/experiencia.png",
+    description: `plataforma de presupuestos que estaba legada con java/jsf bean 
+    a una aplicación cliente-servidor con angular y springboot (entorno de pruebas en desarrollo por la empresa)`,
+    technologies: [
+      {
+        icon: IconKeys.springboot,
+      },
+      {
+        icon: IconKeys.java,
+      },
+      {
+        icon: IconKeys.angular,
+      },
+            {
+        icon: IconKeys.myBatis,
+      },
+            {
+        icon: IconKeys.html,
+      },
+            {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.typeScript,
+      },
+        {
+        icon: IconKeys.material,
+      },
+      
+    ],
+  },
+];
